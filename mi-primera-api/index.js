33e3b7083a74e0df2df45926770c61f935cde8e1
Dagorik <dagorik@gmail.com>
2024-03-11 19:21:10 -0600

    feat: class 2

--- mi-primera-api/index.js
+++ mi-primera-api/index.js
@@ -26,26 +26,29 @@ app.get('/empleados', (req, res) => {
 
 // Obtener todas as mascotas (GET)
 app.get('/mascotas',(req, res) => {
+    console.log(req.query.name);
     const arrayMascota = [
         {id: 1, nombre: 'max'},
         {id: 2, nombre: 'mex'},
     ];
+    
     res.status(200).json(arrayMascota)
-})
+});
 
 // Obtener UNA mascota por ID [mascotas/2] (GET)
-app.get('/mascotas/:id', (req, res) => {
+app.get('/mascotas/:idMascota', (req, res) => {
     // req es todo lo que nos envia el cliente al servidor
     // res es todo lo que el servidor envia al cliente
 
-    const idBusqueda = req.params.id;
+    const idBusqueda = req.params.idMascota;
 
     const arrayMascota = [
         {id: 1, nombre: 'max'},
         {id: 2, nombre: 'mex'},
     ];
     // Busca la mascota por ID
-    const mascotaEncontrada = arrayMascota.find(mascota => mascota.id == idBusqueda)
+    const mascotaEncontrada = arrayMascota.find(mascota => mascota.id == idBusqueda);
+
     if (mascotaEncontrada) {
         res.status(200).json(mascotaEncontrada)
     } else {
@@ -53,10 +56,20 @@ app.get('/mascotas/:id', (req, res) => {
     }
 });
 
-
+// POST (Crear)
+app.post('/mascotas', (req, res)=> {
+    console.log('POST');
+    console.log(req.body);
+    const arrayMascota = [
+        {id: 1, nombre: 'max'},
+        {id: 2, nombre: 'mex'},
+    ];
+    arrayMascota.push({ id: 3, nombre: req.body.nombre });
+    res.status(201).json(arrayMascota);
+});
 
 
 // Levantar el servidor
 app.listen(3000, () => {
-    console.log('El servidor está corriendo correctamente')
+    console.log('El servidor está corriendo correctamente');
 });
