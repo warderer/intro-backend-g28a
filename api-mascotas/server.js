999b000d96b975e28ff7195648ceb94ddec00b11
Dagorik <dagorik@gmail.com>
2024-03-13 19:35:13 -0600

    feat: first post

--- api-mascotas/server.js
+++ api-mascotas/server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 
+// Mandando a llamar la conexión a la base de datos
+const db = require('./config');
+
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Llamas en pijamas')
@@ -11,6 +17,18 @@ app.get('/', (req, res) => {
 
 // Create
 app.post('/api/mascotas', (req, res) => {
+    const mascota = req.body;
+    // Guardar en la base de datos
+    db
+        .insert(mascota)
+        .into('mascotas')
+        .returning(['mascota_id', 'name', 'breed', 'age', 'active'])
+        .then((newMascota) => {
+            res.status(201).json(newMascota);
+        }).catch((err) => {
+            console.log(err);
+            res.status(400).json({ error: 'Tuvimos un error, intenta más tarde' });
+        });
 
 });
 
